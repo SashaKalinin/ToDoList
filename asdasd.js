a85133b76bc7d7aa80d7c49a059049bbba76b7d2
SashaKalinin <sashakalininweb@gmail.com>
2020-05-21 21:54:58 +0300

    123

--- asdasd.js
+++ asdasd.js
@@ -1,17 +1,24 @@
-var addBtn = document.getElementById('addTask');
-var inputTittle = document.querySelector('#inputTitle').value;
-var inputext = document.querySelector('#inputText').value;
-var currentTasks = document.querySelector('#currentTasks');
-var completedTasks = document.querySelector('#completedTasks');
+const addBtn = document.getElementById('addTask');
+const inputTittle = document.querySelector('#inputTitle').value;
+const inputext = document.querySelector('#inputText').value;
+const currentTasks = document.querySelector('#currentTasks');
+const completedTasks = document.querySelector('#completedTasks');
 
 let todoList = [];
 
 
-function displayToDo() {
+// parse localstorage
+// if (localStorage.getItem('todo')) {
+//     todoList = JSON.parse(localStorage.getItem('todo'));
+//     displayMessages();
+// }
+
+//display todo
+function displayMessages() {
     currentTasks.innerHTML = '';
     if (todoList.length === 0) currentTasks.innerHTML = '';
-    todoList.forEach(function (item) {
-        currentTasks.innerHTML += `  <li class="list-group-item d-flex w-100 mb-2" id="${item.id}">
+    todoList.forEach(function (item, i) {
+        currentTasks.innerHTML += `  <li class="list-group-item d-flex w-100 mb-2" id="item_${i}">
         <div class="w-100 mr-2">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">${item.tittle}</h5>
@@ -28,9 +35,9 @@ function displayToDo() {
                 <i class="fas fa-ellipsis-v"></i>
             </button>
             <div class="dropdown-menu p-2 flex-column" aria-labelledby="dropdownMenuItem1">
-                <button type="button" class="btn btn-success w-100" id="${item.id}">Complete</button>
-                <button type="button" class="btn btn-info w-100 my-2" id="${item.id}">Edit</button>
-                <button type="button" class="btn btn-danger w-100" id="${item.id}">Delete</button>
+                <button type="button" class="btn btn-success w-100" id="item_${i}">Complete</button>
+                <button type="button" class="btn btn-info w-100 my-2" id="item_${i}">Edit</button>
+                <button type="button" class="btn btn-danger w-100" id="item_${i}">Delete</button>
             </div>
         </div>
     </li>`;
@@ -39,6 +46,7 @@ function displayToDo() {
 };
 
 
+//choose radio
 function chooseRadio(radios) {
     for (let i = 0; i < radios.length; i++) {
         if (radios[i].checked) {
@@ -57,29 +65,30 @@ function closeModal() {
     document.body.removeChild(modalBackdrops[0]);
 }
 
-//addToDo
+
+//addtodo
 addBtn.addEventListener('click', function (event) {
     event.preventDefault();
     let newTodo = {
-        'id': Math.random(),
+        // 'id': todoList[todoList.length-1].id + 1,
         'tittle': document.querySelector('#inputTitle').value,
         'text': document.querySelector('#inputText').value,
         "pripority": chooseRadio(document.querySelectorAll('input[name=gridRadios]')).value,
         "date": new Date(),
         'complete': false
     };
+    
     if (document.querySelector('#inputTitle').value && document.querySelector('#inputText').value) {
+
         todoList.push(newTodo);
-        displayToDo();
+        displayMessages();
+        // localStorage.setItem('todo', JSON.stringify(todoList));
         document.querySelector('#inputTitle').value = '';
         document.querySelector('#inputText').value = '';
         closeModal();
-        
     }
-
-
-})
-
+    
+});
 
 //complete
 currentTasks.addEventListener('click', function (e) {
@@ -88,8 +97,11 @@ currentTasks.addEventListener('click', function (e) {
             if (e.target.id == item.id) {
                 completedTasks.appendChild(item);
                 //добавляем в массив выполненные
-              
-
+                todoList.splice(item,1);
+                completeList.push(item);
+                console.log(item);
+                // localStorage.setItem('complete', JSON.stringify(completeList));
+                
             }
         }
         //DELTE
@@ -98,17 +110,14 @@ currentTasks.addEventListener('click', function (e) {
             if (e.target.id == item.id) {
                 item.remove();
                 for(let i = 0; i < todoList.length; i++) {
-                    if(e.target.id == todoList[i].id) {
-                        todoList.splice(i,1);
-                        console.log(todoList);
+                    if(todoList[i].id == e.target.id){
+                        console.log(1);
                     }
                 }
             }
-        };
+        }
 
-
-        
-    } else if (e.target.innerHTML == 'Edit') {
+    }else if (e.target.innerHTML == 'Edit') {
         for (let item of currentTasks.children) {
             if (e.target.id == item.id) {
                 document.querySelector('#addTask2').click();
@@ -126,18 +135,9 @@ completedTasks.addEventListener('click', function (e) {
         for (let item of completedTasks.children) {
             if (e.target.id == item.id) {
                 item.remove();
-                for(let i = 0; i < todoList.length; i++) {
-                    if(e.target.id == todoList[i].id) {
-                        todoList.splice(i,1);
-                        console.log(todoList);
-                    }
-                }
-               
             }
-        };
+        }
 
-
-        
     }
 
 });
@@ -147,11 +147,22 @@ document.querySelector('#des').addEventListener('click', function () {
     todoList = todoList.sort(function (a, b) {
         return a.date - b.date;
     });
-    displayToDo();
+    displayMessages();
 });
 document.querySelector('#asc').addEventListener('click', function () {
     todoList = todoList.sort(function (a, b) {
         return b.date - a.date;
     });
-    displayToDo();
+    displayMessages();
 });
+
+
+//SHOWMODAL
+function showModal(){
+    let modal = document.querySelector('#exampleModal');
+    modal.classList.add('show');
+    modal.setAttribute('aria-hidden', 'false');
+    modal.setAttribute('style', 'display: block');
+    const modalBackdrops = document.getElementsByClassName('modal-backdrop');
+    document.body.removeChild(modalBackdrops[1]);
+}
